95a6690c019ac2758f4bab2c11d93831168cc721
panukettu <panu@0xp.lol>
2023-04-22 21:20:39 +0300

    fix remappings and artifacts

--- sol/contracts/Ghost.sol
+++ sol/contracts/Ghost.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import { ERC20 } from 'solmate/erc20/ERC20.sol';
-import './interfaces/IGhost.sol';
+import { ERC20 } from 'solmate/tokens/ERC20.sol';
+import { IGhost } from './interfaces/IGhost.sol';
 
 contract Ghost is IGhost {
   function boo() external pure returns (string memory) {
