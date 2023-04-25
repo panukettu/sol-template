90f18b4c33caa86568ecdfdab858774a6b3ccef2
panukettu <panu@0xp.lol>
2023-04-25 22:04:12 +0300

    add wallet helper

--- sol/scripts/Bridge.s.sol
+++ sol/scripts/Bridge.s.sol
@@ -3,13 +3,15 @@ pragma solidity >=0.8.18;
 
 import { Script } from 'forge-std/Script.sol';
 import { IPolygonZkEvmBridge, PolygonZkEvm, Arbitrum, Optimism, IArbitrumBridge, IOPBridge } from '../contracts/interfaces/IBridges.sol';
+import { TestWallets } from './Wallet.s.sol';
 
-contract BridgeZkEvm is Script {
+string constant key = 'PRIVATE_KEY';
+
+contract BridgeZkEvm is TestWallets {
   IPolygonZkEvmBridge internal zkEvmBridge;
 
-  function run() external {
-    vm.startBroadcast(vm.envUint('PRIVATE_KEY_EXTERNAL'));
-    address user = vm.addr(vm.envUint('PRIVATE_KEY_EXTERNAL'));
+  function run() external broadcastWithKey(key) {
+    address user = getAddr(key);
     zkEvmBridge = IPolygonZkEvmBridge(PolygonZkEvm.BRIDGE_GOERLI);
     uint256 toBridge = 1 ether;
 
@@ -21,62 +23,57 @@ contract BridgeZkEvm is Script {
       true,
       ''
     );
-    vm.stopBroadcast();
   }
 }
 
-contract BridgeArbitrum is Script {
+contract BridgeArbitrum is TestWallets {
   IArbitrumBridge internal arbitrumBridge;
 
-  function run() external {
-    vm.startBroadcast(vm.envUint('PRIVATE_KEY_EXTERNAL'));
-    // address user = vm.addr(vm.envUint('PRIVATE_KEY_EXTERNAL'));
-
+  function run() external broadcastWithKey(key) {
     arbitrumBridge = IArbitrumBridge(Arbitrum.BRIDGE_GOERLI);
     uint256 toBridge = 0.1 ether;
 
     arbitrumBridge.depositEth{ value: toBridge }();
-    vm.stopBroadcast();
   }
 }
 
-contract BridgeOptimism is Script {
+contract BridgeOptimism is TestWallets {
   IOPBridge internal opBridge;
 
-  function run() external {
-    vm.startBroadcast(vm.envUint('PRIVATE_KEY_EXTERNAL'));
-    address to = vm.addr(vm.envUint('PRIVATE_KEY_EXTERNAL'));
+  function run() external broadcastWithKey(key) {
+    address to = getAddr(key);
 
     opBridge = IOPBridge(Optimism.BRIDGE_GOERLI);
-    uint256 toBridge = 0.1 ether;
+    uint256 amount = 0.1 ether;
 
-    opBridge.depositETHTo{ value: toBridge }(to, 200000, '');
-    vm.stopBroadcast();
+    opBridge.depositETHTo{ value: amount }(to, 200000, '');
   }
 }
 
-contract BridgeAll is Script {
+contract BridgeAll is TestWallets {
   IOPBridge internal opBridge;
   IArbitrumBridge internal arbitrumBridge;
   IPolygonZkEvmBridge internal zkEvmBridge;
 
   function run() external {
-    vm.startBroadcast(vm.envUint('PRIVATE_KEY_EXTERNAL'));
-    address to = vm.addr(vm.envUint('PRIVATE_KEY_EXTERNAL'));
+    address to = getAddr(0);
+    uint256 amount = 0.05 ether;
+    sendToAllBridges(amount, to);
+  }
+
+  function sendToAllBridges(uint256 _amountToBridge, address _to) public broadcastWithMnemonic(0) {
+    address to = _to == address(0) ? getAddr(0) : _to;
 
     opBridge = IOPBridge(Optimism.BRIDGE_GOERLI);
     arbitrumBridge = IArbitrumBridge(Arbitrum.BRIDGE_GOERLI);
     zkEvmBridge = IPolygonZkEvmBridge(PolygonZkEvm.BRIDGE_GOERLI);
 
-    uint256 toBridge = 0.1 ether;
-
-    arbitrumBridge.depositEth{ value: toBridge }();
-    opBridge.depositETHTo{ value: toBridge }(to, 200000, '');
-
-    zkEvmBridge.bridgeAsset{ value: toBridge }(
+    arbitrumBridge.depositEth{ value: _amountToBridge }();
+    opBridge.depositETHTo{ value: _amountToBridge }(to, 200000, '');
+    zkEvmBridge.bridgeAsset{ value: _amountToBridge }(
       PolygonZkEvm.ZKEVM_ID,
       to,
-      toBridge,
+      _amountToBridge,
       address(0),
       true,
       ''
